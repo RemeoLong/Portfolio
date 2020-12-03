d131d45cc172edbbcaa519a98cff7f854f90dc5a
remeolong <remeolong82@yahoo.com>
2020-12-03 17:04:33 -0600

    Html Questions

--- SAT/views.py
+++ SAT/views.py
@@ -1,8 +1,8 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader
-
-from .models import Question
-from django.shortcuts import render
+from django.urls import reverse
+from .models import Question, Choice
+from django.shortcuts import render, get_object_or_404
 
 
 def home(request):
@@ -15,11 +15,8 @@ def test1(request):
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('index/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'index/index.html', context)
 
 
 def test2(request):
@@ -59,11 +56,32 @@ def tips(request):
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question%s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Sorry, Question does not exist")
+    return render(request, 'index/details.html', {'question': question})
 
 
 def result(request, question_id):
-    response = "You're looking at the results of question%s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'index/results.html', {'question': question})
 
 
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'index/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('SAT:results', args=(question.id,)))
