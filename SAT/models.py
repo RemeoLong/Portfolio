bb066b7a3282dcaa94c19fd4e9d93debb660f26e
remeolong <remeolong82@yahoo.com>
2020-12-04 12:15:33 -0600

    model update

--- SAT/models.py
+++ SAT/models.py
@@ -10,22 +10,30 @@ class Passage(models.Model):
         return self.passage_text
 
 
+class Explanation(models.Model):
+    explain_text = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.explain_text
+
+
 class Question(models.Model):
     passage = models.ForeignKey(Passage, on_delete=models.CASCADE)
     questions_text = models.CharField(max_length=400)
-    pub_date = models.DateTimeField('date published')
 
     def __str__(self):
         return self.questions_text
 
-    def published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-
 
 class Choice(models.Model):
+    correct_choices = (
+        ("Correct", "Correct"),
+        ("Incorrect", "Incorrect"),
+    )
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    explain = models.ForeignKey(Explanation, on_delete=models.CASCADE, default="")
     choice_text = models.CharField(max_length=200)
-    is_answer = models.BooleanField(default=False)
+    correct = models.CharField(max_length=10, choices=correct_choices, default='Incorrect')
 
     def __str__(self):
         return self.choice_text
@@ -37,11 +45,8 @@ class Choice(models.Model):
         return self.choices_set.filter(is_answer=True)
 
 
-class Explanation(models.Model):
-    choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
-    explain = models.CharField(max_length=500)
 
-    def __str__(self):
-        return self.explain
+
+
 
 
