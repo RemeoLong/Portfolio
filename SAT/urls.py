879ee3230a4a47eace5dcca906f066f1e54df61d
remeolong <remeolong82@yahoo.com>
2021-01-01 16:06:40 -0600

    Portfolio update to center the pages

--- SAT/urls.py
+++ SAT/urls.py
@@ -12,10 +12,10 @@ urlpatterns = [
     path('/Test/<int:test_id>/Section/<int:section_id>', SectionList.as_view(), name='SectionList'),
 
 
-    path('/Test/<int:test_id>/Section/Reading', views.reading, name='Reading'),
-    path('/Test/<int:test_id>/Section/Writing', views.writing, name='Writing'),
-    path('/Test/<int:test_id>/Section/MathNC', views.mathnc, name='MathNC'),
-    path('/Test/<int:test_id>/Section/Math', views.math, name='Math'),
+    path('/Test/<int:test_id>/Section/Reading/<int:section_id>', views.reading, name='Reading'),
+    path('/Test/<int:test_id>/Section/Writing/<int:section_id>', views.writing, name='Writing'),
+    path('/Test/<int:test_id>/Section/MathNC/<int:section_id>', views.mathnc, name='MathNC'),
+    path('/Test/<int:test_id>/Section/Math/<int:section_id>', views.math, name='Math'),
 
 
     path('/Test/<int:test_id>/Section/<int:section_id>/Question/<int:question_id>/', views.detail, name='Question'),
