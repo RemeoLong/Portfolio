8d5f1cbd13c1b6e68ecca273ed78978d097f0b4f
remeolong <remeolong82@yahoo.com>
2021-02-02 12:44:13 -0600

    Switched projects order

--- SAT/views.py
+++ SAT/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.urls import reverse
-from .models import Question, Choice, Test, Section, Answer
+from .models import Question, Choice, Test, Section, Answer, TotalScore
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login, authenticate
